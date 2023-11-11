ca71dd9b021a914f0e218d9ed2da3ea01bd31736
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:53:58 +0000

    Updated program to use logging instead of passing data to a text file

--- TempParse.py
+++ TempParse.py
@@ -1,35 +1,45 @@
+import logging
 import psutil
 import sys
 import time
-from datetime import datetime
 import textwrap
+from distutils.debug import DEBUG
+import datetime
+import logging
 
-#Gets unix timestamp
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+def LOGGINGINIT():
+
+    
+    logging.basicConfig (filename= 'Output.log', encoding='utf-8', level=logging.DEBUG)
+    
+    LOGEVENTS_INFO(f"Logging started")
+    
+
+def LOGEVENTS_INFO(input):
+
+    currentdateandtime = datetime.datetime.now()
+    currentdateandtimestr = currentdateandtime.strftime("%m/%d/%Y, %H:%M:%S")
+    
+    logging.info(f"{currentdateandtimestr} " + input)
 
 
-# Returns system wide Usages
-cpuUsage = psutil.cpu_percent(interval=1)
-diskUsage = psutil.disk_usage('/')
-memoryUsage = psutil.virtual_memory()
-Sensors = psutil.sensors_temperatures(fahrenheit= 0)
-
-cpuusagestring = str(cpuUsage)
-diskusagestring = str(diskUsage)
-memoryusagestring = str(memoryUsage)
-sensorsstring = str(Sensors)
-
-
+LOGGINGINIT()
 
 while True:
-    sys.stdout = open("Usage.txt", "a")
-    print("-------------"+ dt_string + "-------------")
-    print(" Cpu Usage = " + cpuusagestring)
-    print(" Disk Usage = " + diskusagestring)
-    print(" Memory Usage = " + memoryusagestring)
-    print(" Temperatures = " + sensorsstring)
-    print("---------------------------------------------")
-    time.sleep(1800)
+    # Returns system wide Usages
+    cpuUsage = psutil.cpu_percent(interval=1)
+    diskUsage = psutil.disk_usage('/')
+    memoryUsage = psutil.virtual_memory()
+    Sensors = psutil.sensors_temperatures(fahrenheit= 0)
 
+    cpuusagestring = str(cpuUsage)
+    diskusagestring = str(diskUsage)
+    memoryusagestring = str(memoryUsage)
+    sensorsstring = str(Sensors)
+
+    LOGEVENTS_INFO(f"CPU Usage: " + cpuusagestring)
+    LOGEVENTS_INFO(f"Disk Usage: " + diskusagestring)
+    LOGEVENTS_INFO(f"Memory Usage: " + memoryusagestring)
+    LOGEVENTS_INFO(f"Temperatures: " + sensorsstring)
+    time.sleep(60)
 
